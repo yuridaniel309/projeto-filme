ceb5ac12baa9bcb07c120fd8f6f933d78e226e20
yuri daniel <yuridanielsantana45@gmail.com>
2025-10-07 16:47:51 -0300

    funcao inserir filmes

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -1,15 +1,15 @@
 from conexao import conectar
 def criar_tabela():
-    conexao, cursor = conectar
+    conexao, cursor = conectar()
     if conexao:
         try:
             cursor.execute("""
-             CREATE TABLE IF NOT EXISTS filmes(
-              id  serisl primary key    
-               titulo TEXT NOT NULL
-               genero TEXT NOT NULL
-              ano INTEGER NOT NULL
-              avaliacao REAL)
+            CREATE TABLE IF NOT EXISTS filmes(
+                id  serial PRIMARY  KEY,
+                titulo TEXT NOT NULL,
+                genero TEXT NOT NULL,
+                ano INTEGER NOT NULL,
+                avaliacao REAL)
             """)
             conexao.commit()
         except Exception as erro:
@@ -17,5 +17,26 @@ def criar_tabela():
         finally:
             cursor.close()
             conexao.close()
-            criar_tabela()
+criar_tabela()
+
+def inserir_fime(titulo,genero,ano,avaliacao):
+    conexao,cursor =conectar()
+    if conexao:
+        try:
+            cursor.execute(
+            "INSERT INTO filmes (titulo genero, ano, avaliacao)VALUES(%s,%s,%s,%s)",
+            (titulo,genero,ano,avaliacao)
+            )
+
+            conexao.commit()
+        except Exception as  erro:
+            print(f'erro ao inserir filmes')
+        finally:
+            cursor.close()
+            conexao.close()
+            inserir_fime("avatar","acao",2009,10,0)
+            
+
+
+        
             
